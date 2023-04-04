28ea2b7fededbd8cf7d090c6d772a9c7c72d8556
Francois <f.jourden@live.fr>
2023-04-04 19:42:11 +0200

    add refresh and element click animation on books list

--- lib/components/libraryScreen/grid_book_element.dart
+++ lib/components/libraryScreen/grid_book_element.dart
@@ -4,8 +4,7 @@ import 'package:my_library/utils.dart';
 import 'package:sqflite/sqflite.dart';
 
 class GridBookElement extends StatelessWidget {
-  const GridBookElement({Key? key, required this.book, required this.db})
-      : super(key: key);
+  const GridBookElement({Key? key, required this.book, required this.db}) : super(key: key);
   final Book book;
   final Database db;
 
@@ -13,23 +12,21 @@ class GridBookElement extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridTile(
       child: GestureDetector(
-        onTap: () => _onclick(context),
         child: Card(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
+          child: InkWell(
+            onTap: () => _onclick(context),
+            borderRadius: BorderRadius.circular(12),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(children: [
                 if (book.cover != null)
-                  Expanded(
-                      child: Image.network(
-                          HttpUtils.getBookCoverLocation(book.cover ?? ""))),
+                  Expanded(child: Image.network(HttpUtils.getBookCoverLocation(book.cover ?? ""))),
                 Padding(
                     padding: const EdgeInsets.only(top: 16),
                     child: Text(book.title, textAlign: TextAlign.justify)),
                 Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: Text('${book.nbPages} pages')),
-              ]
+                    padding: const EdgeInsets.only(top: 16), child: Text('${book.nbPages} pages')),
+              ]),
             ),
           ),
         ),
@@ -38,7 +35,6 @@ class GridBookElement extends StatelessWidget {
   }
 
   void _onclick(BuildContext context) {
-    Navigator.pushNamed(context, '/book_screen',
-        arguments: {'book': book, 'db': db});
+    Navigator.pushNamed(context, '/book_screen', arguments: {'book': book, 'db': db});
   }
 }
--- lib/screens/library_screen.dart
+++ lib/screens/library_screen.dart
@@ -18,40 +18,44 @@ class _LibraryScreenState extends State<LibraryScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _getDbBook(),
-      builder: (BuildContext buildContext,
-          AsyncSnapshot<List<Map<String, Object?>>> snapshot) {
-        if (snapshot.hasData) {
-          List<Book> books = snapshot.data
-                  ?.map(
-                    (book) => Book(
-                        title: book['title'] as String,
-                        isbn: book['isbn'] as String,
-                        nbPages: book['nb_pages'] as int,
-                        cover: book['cover'] as String?),
-                  )
-                  .toList() ??
-              [];
-          return GridView.builder(
-              itemCount: snapshot.data?.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, childAspectRatio: 0.75),
-              itemBuilder: (_, int index) =>
-                  GridBookElement(book: books[index], db: widget.db));
-        }
-        if (snapshot.hasError) {
-          return Center(
-            child: Column(children: [
-              const Icon(Icons.error_outline, color: Colors.red, size: 60),
-              Text(snapshot.error.toString())
-            ]),
-          );
-        }
-        return const Center(
-            child: SizedBox(
-                width: 60, height: 60, child: CircularProgressIndicator()));
-      },
-    );
+    return RefreshIndicator(
+        onRefresh: ()=> _onRefresh(),
+        child: FutureBuilder(
+          future: _getDbBook(),
+          builder: (BuildContext buildContext, AsyncSnapshot<List<Map<String, Object?>>> snapshot) {
+            if (snapshot.hasData) {
+              List<Book> books = snapshot.data
+                      ?.map(
+                        (book) => Book(
+                            title: book['title'] as String,
+                            isbn: book['isbn'] as String,
+                            nbPages: book['nb_pages'] as int,
+                            cover: book['cover'] as String?),
+                      )
+                      .toList() ??
+                  [];
+              return GridView.builder(
+                  itemCount: snapshot.data?.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, childAspectRatio: 0.75),
+                  itemBuilder: (_, int index) =>
+                      GridBookElement(book: books[index], db: widget.db));
+            }
+            if (snapshot.hasError) {
+              return Center(
+                child: Column(children: [
+                  const Icon(Icons.error_outline, color: Colors.red, size: 60),
+                  Text(snapshot.error.toString())
+                ]),
+              );
+            }
+            return const Center(
+                child: SizedBox(width: 60, height: 60, child: CircularProgressIndicator()));
+          },
+        ));
+  }
+
+  Future<void> _onRefresh() async{
+    setState(() {});
   }
 }
